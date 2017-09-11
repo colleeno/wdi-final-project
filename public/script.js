ab60a48bf140c70a925f23cc6abd5f267ef9e60f
Colleen O'Reilly <colleenaoreilly@gmail.com>
2017-09-11 19:29:38 -0400

    set up results from autocomplete city search and lat long for weather call

--- public/script.js
+++ public/script.js
@@ -1,67 +1,56 @@
-console.log('linked')
-
 $(document).ready(function () {
 
-var search
-var url
-var long
-var lat
-var outdoor = ['museum', 'park', 'restaurant']
-var indoor = ['spa', 'shopping_mall', 'restaurant']
-var categoryTerm
+  var outdoor = ['museum', 'park', 'restaurant']
+  var indoor = ['spa', 'shopping_mall', 'restaurant']
+  var map
+  var service
 
-$('#search').submit(function(e){
-  e.preventDefault()
-  search = $('input').val()
+  var input = document.getElementById('autocomplete');
+  var autocomplete = new google.maps.places.Autocomplete(input,{types: ['(cities)']})
 
-  $.ajax({
-    url: `http://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${secondKey}&units=imperial`,
-    type: 'get',
-    dataType: 'json'
-      }).done((response) => {
-        console.log('weather request success!')
-        long = response.coord.lon
-        lat = response.coord.lat
-        latlngVal = `${response.coord.lat}, ${response.coord.lon}`
-        $('<div></div>').appendTo('body').text(`temp is ${response.main.temp}° F`)
-        $('<div></div>').appendTo('body').text(`${response.weather[0].description}`)
-        if (response.weather[0].main == 'Rain' || parseInt(response.main.temp) < 60 ) {
-          $('<div></div>').appendTo('body').text(`${indoor[0]}`).addClass("category")
-          $('<div></div>').appendTo('body').text(`${indoor[1]}`).addClass("category")
-          $('<div></div>').appendTo('body').text(`${indoor[2]}`).addClass("category")
-        }
-        else {
-          $('<div></div>').appendTo('body').text(`${outdoor[0]}`).addClass("category")
-          $('<div></div>').appendTo('body').text(`${outdoor[1]}`).addClass("category")
-          $('<div></div>').appendTo('body').text(`${outdoor[2]}`).addClass("category")
-        }
-        $('.category').on('click', initialize)
-      }).fail(() => {
-        console.log('weather request failed')
-      }).always(() => {
-        console.log('This always happens regardless of successful ajax request or not.')
-      })
+  google.maps.event.addListener(autocomplete, 'place_changed', function(){
+    city = autocomplete.getPlace()
+    lat = city.geometry.location.lat()
+    lng = city.geometry.location.lng()
+        $.ajax({
+          url: `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${secondKey}&units=imperial`,
+          type: 'get',
+          dataType: 'json'
+            }).done((response) => {
+              console.log('new weather request success!')
+                $('<div></div>').appendTo('body').text(`temp is ${response.main.temp}° F`)
+                $('<div></div>').appendTo('body').text(`${response.weather[0].description}`)
+              if (response.weather[0].main == 'Rain' || parseInt(response.main.temp) < 60 ) {
+                $('<div></div>').appendTo('body').text(`${indoor[0]}`).addClass("category")
+                $('<div></div>').appendTo('body').text(`${indoor[1]}`).addClass("category")
+                $('<div></div>').appendTo('body').text(`${indoor[2]}`).addClass("category")
+              }
+              else {
+                $('<div></div>').appendTo('body').text(`${outdoor[0]}`).addClass("category")
+                $('<div></div>').appendTo('body').text(`${outdoor[1]}`).addClass("category")
+                $('<div></div>').appendTo('body').text(`${outdoor[2]}`).addClass("category")
+              }
+              $('.category').on('click', initialize)
+            }).fail(() => {
+              console.log('new weather request failed')
+            }).always(() => {
+              console.log('log regardless of ajax success or fail')
+            })
+
     })
 
-    var map;
-    var service;
-    var infowindow;
-    var latlngVal
-
     function initialize() {
-      var type = $(this).text()
-      var location = new google.maps.LatLng(lat,long);
-
+      var categoryType = $(this).text()
+      var location = new google.maps.LatLng(lat,lng)
       map = new google.maps.Map(document.getElementById('results'), {
-        });
+        })
 
       var request = {
         location: location,
         radius: '2000',
-        type: type,
+        type: categoryType,
         rankBy: google.maps.places.RankBy.PROMINENCE
-        // rankby : google.maps.places.RankBy.DISTANCE
-      };
+      }
 
       service = new google.maps.places.PlacesService(map);
       service.nearbySearch(request, callback);
@@ -74,7 +63,7 @@ $('#search').submit(function(e){
           service.getDetails({
           placeId: place.place_id
           }, function(place, status) {
-            console.log(place)
+            // console.log(place)
             $(`<p>${place.name}</p>`).appendTo('body')
             $(`<p>${place.formatted_address}</p>`).appendTo('body')
             $(`<a href="${place.website}">${place.website}</a>`).appendTo('body')
