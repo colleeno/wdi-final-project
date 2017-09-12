e9b51b19fc94f5e52af16a80aedd6904f9b44fcc
Colleen O'Reilly <colleenaoreilly@gmail.com>
2017-09-12 10:04:57 -0400

    hidden weather key

--- public/script.js
+++ public/script.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
   var indoor = ['spa', 'shopping_mall', 'restaurant']
   var map
   var service
+  var secondKey = $('.hidden-key').text()
 
   var input = document.getElementById('autocomplete');
   var autocomplete = new google.maps.places.Autocomplete(input,{types: ['(cities)']})
