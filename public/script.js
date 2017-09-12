d01687367768b6e3d73447f532336823380b6109
Colleen O'Reilly <colleenaoreilly@gmail.com>
2017-09-12 18:04:44 -0400

    randomized and sliced list of possible activites

--- public/script.js
+++ public/script.js
@@ -7,8 +7,8 @@ $(document).ready(function () {
   var secondKey = $('.hidden-key').text()
 
   // set up search form to autocomplete
-  var input = document.getElementById('autocomplete');
-  var autocomplete = new google.maps.places.Autocomplete(input,{types: ['(cities)']})
+  var input = document.getElementById('autocomplete')
+  var autocomplete = new google.maps.places.Autocomplete(input, {types: ['(cities)']})
 
   //listen for form to change
   google.maps.event.addListener(autocomplete, 'place_changed', function(){
@@ -18,22 +18,21 @@ $(document).ready(function () {
     lat = city.geometry.location.lat()
     lng = city.geometry.location.lng()
       //connect to weather api with lat and lng
-        $.ajax({
-          // url: `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${secondKey}&units=imperial`,
-          url: `http://api.apixu.com/v1/forecast.json?key=${secondKey}&q=${lat},${lng}`,
-          type: 'get',
-          dataType: 'json'
-          }).done((response) => {
-              console.log('weather request success!')
-              //if weather request is successful, run displayWeather function
-              displayWeather(response)
-              //when user clicks on category name, run function to get places
-              $('.category').on('click', getPlaces)
-          }).fail(() => {
-              console.log('new weather request failed')
-          }).always(() => {
-              console.log('log regardless of ajax success or fail')
-          })
+      $.ajax({
+        url: `http://api.apixu.com/v1/forecast.json?key=${secondKey}&q=${lat},${lng}`,
+        type: 'get',
+        dataType: 'json'
+        }).done((response) => {
+            console.log('weather request success!')
+            //if weather request is successful, run displayWeather function
+            displayWeather(response)
+            //when user clicks on category name, run function to get places
+            $('.category').on('click', getPlaces)
+        }).fail(() => {
+            console.log('new weather request failed')
+        }).always(() => {
+            console.log('log regardless of ajax success or fail')
+        })
     })
 
 
@@ -53,32 +52,30 @@ $(document).ready(function () {
   function mapWeatherToActivities () {
     //if rain, cold, or too hot do indoor
     if (currentDescription.toLowerCase().indexOf('rain') !== -1 || dailyDescription.toLowerCase().indexOf('rain') !== -1 || feelslikeTemp < 60 || feelslikeTemp > 100) {
-      console.log('do indoor activity')
-      $('<div></div>').appendTo('body').text(`${indoor[1][0]}`).addClass(`${indoor[1][1]}`)
+      randomizeActivities(indoor)
+      randomizeList.forEach(function(activity) {
+        $('<div></div>').appendTo('body').text(`${activity[0]}`).addClass(`category ${activity[1]}`)
+      })
     }
     //otherwise do outdoor
     else {
       console.log('do outdoor activity')
-      $('<div></div>').appendTo('body').text(`${outdoor[0][0]}`).addClass(`${outdoor[0][1]}`)
-    }}
+      randomizeActivities(outdoor)
+      randomizeList.forEach(function(activity) {
+        $('<div></div>').appendTo('body').text(`${activity[0]}`).attr({'class': 'category', 'id': `${activity[0]}`})
+      })
+    }
+  };
 
-  //   $('<div></div>').appendTo('body').text(`temp is ${response.main.temp}° F`)
-  //   $(`<img src="http://openweathermap.org/img/w/${response.weather[0].icon}.png" />`).appendTo('body')
-  //   $('<div></div>').appendTo('body').text(`${response.weather[0].description}`)
-  // if (response.weather[0].main == 'Rain' || parseInt(response.main.temp) < 60 ) {
-  //   $('<div></div>').appendTo('body').text(`${indoor[0]}`).addClass("category")
-  //   $('<div></div>').appendTo('body').text(`${indoor[1]}`).addClass("category")
-  //   $('<div></div>').appendTo('body').text(`${indoor[2]}`).addClass("category")
-  // } else {
-  //   $('<div></div>').appendTo('body').text(`${outdoor[0]}`).addClass("category")
-  //   $('<div></div>').appendTo('body').text(`${outdoor[1]}`).addClass("category")
-  //   $('<div></div>').appendTo('body').text(`${outdoor[2]}`).addClass("category")
-  //   }
+  function randomizeActivities (activityList) {
+   randomizeList = activityList.sort(function(a, b){return 0.5 - Math.random()}).slice(0,2)
+   return randomizeList
+ }
 
 // find places from google places
   function getPlaces() {
     //set type to search as category selected by user
-    var categoryType = $(this).text()
+    var categoryType = $(this).attr('id')
     //search by locations lat lng values
     var location = new google.maps.LatLng(lat,lng)
     // must create map though it wont be displayed in this app
@@ -106,12 +103,16 @@ $(document).ready(function () {
   }
   // display place search result details on page
   function displayPlaces (place, status) {
-    if (place != null) {
+    if (place !==  'undefined' && place) {
     $(`<p>${place.name}</p>`).appendTo('body')
     $(`<p>${place.formatted_address}</p>`).appendTo('body')
-    $(`<a href="${place.website}">${place.website}</a>`).appendTo('body')
+    // edit site to link whole section or add logic to show if there
+    $(`website: <a href="${place.website}">${place.website}</a>`).appendTo('body')
+    // add logic to round rating and show star image possibly
     $(`<p>${place.rating} stars</p>`).appendTo('body')
-    $(`<p>${place.reviews[0].text}</p>`).appendTo('body')
+      if (place.reviews !== 'undefined') {
+        {$(`<p>review: ${place.reviews[0].text}</p>`).appendTo('body')}
+      }
     }
   }
 
