23399cd09b64f0760f7e8ac9072eec203bd2d50d
Colleen O'Reilly <colleenaoreilly@gmail.com>
2017-09-13 19:34:34 -0400

    updated styling for weather display

--- public/script.js
+++ public/script.js
@@ -1,20 +1,19 @@
 $(document).ready(function () {
 
-  var indoor = [['spa', 'spa'], ['shopping', 'retail store'], ['restaurant', 'restaurant'], ['museum', 'museum'], ['movies', 'movies']]
-  var outdoor = [['museum', 'museum'], ['park', 'park'], ['restaurant', 'fine dining'], ['biking', 'biking'], ['golf', 'golf course'],
-  ['bowling', 'bowling lane'], ['spa', 'spa'], ['shopping', 'department store'], ['movies', 'movies']]
+  var indoor = [['spa', 'spa'], ['shopping', 'department_store'], ['restaurant', 'fine dining'], ['museum', 'museum'], ['bowling', 'bowling lane'], ['movies', 'movies']]
+  var outdoor = [['museum', 'museum'], ['park', 'park'], ['restaurant', 'fine dining'], ['biking', 'biking'], ['golf', 'golf course'], ['zoo', 'zoo']]
   var map
   var service
   var secondKey = $('.hidden-key').text()
 
-  // set up search form to autocomplete
-  var input = document.getElementById('autocomplete')
-  var autocomplete = new google.maps.places.Autocomplete(input, {types: ['(cities)']})
+  // set up city search to autocomplete
+  var input = document.getElementById('citySearch')
+  var citySearch = new google.maps.places.Autocomplete(input, {types: ['(cities)']})
 
   //listen for form to change
-  google.maps.event.addListener(autocomplete, 'place_changed', function(){
+  google.maps.event.addListener(citySearch, 'place_changed', function(){
     //complete form with cities only
-    city = autocomplete.getPlace()
+    city = citySearch.getPlace()
     //get latitude and longitude for location
     lat = city.geometry.location.lat()
     lng = city.geometry.location.lng()
@@ -42,35 +41,45 @@ $(document).ready(function () {
     feelslikeTemp = Math.round(response.current.feelslike_f)
     currentDescription = response.current.condition.text
     dailyDescription = response.forecast.forecastday[0].day.condition.text
-    $('<div></div>').appendTo('body').text(`temp is ${currentTemp}° F`)
-    $('<div></div>').appendTo('body').text(`feels like ${feelslikeTemp}° F`)
-    $('<div></div>').appendTo('body').text(`current: ${currentDescription}`)
-    $('<div></div>').appendTo('body').text(`today's forecast: ${dailyDescription}`)
+    $('<p class="temp"></p>').prependTo('.weather').text(`${currentTemp}°`)
+    $('<p></p>').appendTo('.weatherDetails').text(`Feels like ${feelslikeTemp}°`)
+    $('<p></p>').appendTo('.weatherDetails').text(`Current: ${currentDescription}`)
+    $('<p></p>').appendTo('.weatherDetails').text(`Today's forecast: ${dailyDescription}`)
     //recommend activities based on weather
     mapWeatherToActivities()
+    scrollWeather()
   }
 
+  function scrollWeather (){
+    $('html, body').animate({
+        scrollTop: $('#weatherResults').offset().top
+    }, 1500);
+    return false;
+  }
+
+
   function mapWeatherToActivities () {
-    //if rain, cold, or too hot do indoor
-    if (currentDescription.toLowerCase().indexOf('rain') !== -1 || dailyDescription.toLowerCase().indexOf('rain') !== -1 || feelslikeTemp < 60 || feelslikeTemp > 100) {
+    //if rain, cold, or too hot do indoor, else do outdoor
+    if (currentDescription.toLowerCase().indexOf('rain') !== -1 || dailyDescription.toLowerCase().indexOf('rain') !== -1
+    || currentDescription.toLowerCase().indexOf('mist') !== -1 || dailyDescription.toLowerCase().indexOf('mist') !== -1
+    || currentDescription.toLowerCase().indexOf('drizzle') !== -1 || dailyDescription.toLowerCase().indexOf('drizzle') !== -1
+    || feelslikeTemp < 60 || feelslikeTemp > 100) {
       randomizeActivities(indoor)
+      console.log('indoor')
       randomizeList.forEach(function(activity) {
-        $('<div></div>').appendTo('body').text(`${activity[0]}`).attr({'class': 'category', 'id': `${activity[1]}`})
+        $('<div></div>').appendTo('.categories').text(`${activity[0]}`).attr({'class': 'category', 'id': `${activity[1]}`})
       })
-    }
-    //otherwise do outdoor
-    else {
-      console.log('do outdoor activity')
+    }  else {
+      console.log('outdoot')
       randomizeActivities(outdoor)
       randomizeList.forEach(function(activity) {
-        $('<div></div>').appendTo('body').text(`${activity[0]}`).attr({'class': 'category', 'id': `${activity[1]}`})
+        $('<div></div>').appendTo('.categories').text(`${activity[0]}`).attr({'class': 'category', 'id': `${activity[1]}`})
       })
     }
   };
 
   function randomizeActivities (activityList) {
-   randomizeList = activityList.sort(function(a, b){return 0.5 - Math.random()})
-  //  add back slice after testing .slice(0,2)
+   randomizeList = activityList.sort(function(a, b){return 0.5 - Math.random()}).slice(0,4)
    return randomizeList
  }
 
@@ -81,12 +90,13 @@ $(document).ready(function () {
     //search by locations lat lng values
     var location = new google.maps.LatLng(lat,lng)
     // must create map though it wont be displayed in this app
-    map = new google.maps.Map(document.getElementById('results'), {})
+    map = new google.maps.Map(document.getElementById('map'), {})
     // assign properties to request
     var request = {
       location: location,
       radius: '5000', //meters
       keyword: categoryType, //can be type or keyword
+      exclude: ['liquor_store', 'alcohol', 'hotel', 'health', 'apartment', 'hotel', 'Jiu Jitsu', 'karate'],
       rankBy: google.maps.places.RankBy.PROMINENCE //can also rank by distance
     }
     // perform nearby search with requested props
@@ -107,16 +117,25 @@ $(document).ready(function () {
   function displayPlaces (place, status) {
     if (place !==  'undefined' && place) {
       console.log(place)
-    $(`<p>${place.name}</p>`).appendTo('body')
-    $(`<p>${place.formatted_address}</p>`).appendTo('body')
+    $(`<p>${place.name}</p>`).appendTo('#placeResults')
+    $(`<p>${place.formatted_address}</p>`).appendTo('#placeResults')
     // edit site to link whole section or add logic to show if there
-    $(`<p>website: <a href="${place.website}">${place.website}</a></p>`).appendTo('body')
+    $(`<p>website: <a href="${place.website}">${place.website}</a></p>`).appendTo('#placeResults')
     // add logic to round rating and show star image possibly
-    $(`<p>${place.rating} stars</p>`).appendTo('body')
+    $(`<p>${place.rating} stars</p>`).appendTo('#placeResults')
       if (place.reviews !== 'undefined' && place.reviews[0].text.length > 0) {
-        {$(`<p>review: ${place.reviews[0].text}</p>`).appendTo('body')}
+        {$(`<p>review: ${place.reviews[0].text}</p>`).appendTo('#placeResults')}
       }
+
     }
+    scrollPlace()
+  }
+
+  function scrollPlace (){
+    $('html, body').stop().animate({
+        scrollTop: $('#placeResults').offset().top
+    }, 1500);
+    return false;
   }
 
 })
