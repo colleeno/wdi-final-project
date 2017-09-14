a6a7efaed567b51925b6e64adce99fc77b3f50c2
Colleen O'Reilly <colleenaoreilly@gmail.com>
2017-09-14 12:45:31 -0400

    page clears all results on each new search now

--- public/script.js
+++ public/script.js
@@ -2,17 +2,23 @@ $(document).ready(function () {
 
   var indoor = [['spa', 'spa'], ['shopping', 'department_store'], ['restaurant', 'fine dining'], ['museum', 'museum'], ['bowling', 'bowling lane'], ['movies', 'movies']]
   var outdoor = [['museum', 'museum'], ['park', 'park'], ['restaurant', 'fine dining'], ['biking', 'biking'], ['golf', 'golf course'], ['zoo', 'zoo'], ['ice cream', 'ice cream']]
-  var map
-  var service
   var secondKey = $('.hidden-key').text()
 
-  // set up city search to autocomplete
+//reset page for each new input
+  $('input,textarea').focus(function(){
+     $(this).val('')
+     $('.weather').empty()
+     $('.categories').empty()
+     $('.place').empty()
+     $('#weatherResults').css('display', 'none')
+     $('#placeResults').css('display', 'none')
+  })
+
+  // set city search to autocomplete with cities
   var input = document.getElementById('citySearch')
   var citySearch = new google.maps.places.Autocomplete(input, {types: ['(cities)']})
 
-  //listen for form to change
   google.maps.event.addListener(citySearch, 'place_changed', function(){
-    //complete form with cities only
     city = citySearch.getPlace()
     //get latitude and longitude for location
     lat = city.geometry.location.lat()
@@ -37,16 +43,17 @@ $(document).ready(function () {
 
 
   function displayWeather (response) {
+    // display weather section with results from api
+    $('#weatherResults').css('display', 'block')
     currentTemp = Math.round(response.current.temp_f)
     feelslikeTemp = Math.round(response.current.feelslike_f)
     currentDescription = response.current.condition.text
     dailyDescription = response.forecast.forecastday[0].day.condition.text
-    $('<p class="temp"></p>').prependTo('.weather').text(`${currentTemp}°`)
-    $('<p></p>').appendTo('.weatherDetails').text(`Feels like ${feelslikeTemp}°`)
-    $('<p></p>').appendTo('.weatherDetails').text(`Current: ${currentDescription}`)
-    $('<p></p>').appendTo('.weatherDetails').text(`Today's forecast: ${dailyDescription}`)
-    //recommend activities based on weather
-    $('#weatherResults').css('display', 'block')
+    $('<p class="temp"></p>').appendTo('.weather').text(`${currentTemp}°`)
+    $('<p></p>').appendTo('.weather').text(`Feels like ${feelslikeTemp}°`)
+    $('<p></p>').appendTo('.weather').text(`Current: ${currentDescription}`)
+    $('<p></p>').appendTo('.weather').text(`Today's forecast: ${dailyDescription}`)
+    //find activities based on weather and scroll to results
     mapWeatherToActivities()
     scrollWeather()
   }
@@ -66,12 +73,10 @@ $(document).ready(function () {
     || currentDescription.toLowerCase().indexOf('drizzle') !== -1 || dailyDescription.toLowerCase().indexOf('drizzle') !== -1
     || feelslikeTemp < 60 || feelslikeTemp > 100) {
       randomizeActivities(indoor)
-      console.log('indoor')
       randomizeList.forEach(function(activity) {
         $('<div></div>').appendTo('.categories').text(`${activity[0]}`).attr({'class': 'category', 'id': `${activity[1]}`})
       })
     }  else {
-      console.log('outdoor')
       randomizeActivities(outdoor)
       randomizeList.forEach(function(activity) {
         $('<div></div>').appendTo('.categories').text(`${activity[0]}`).attr({'class': 'category', 'id': `${activity[1]}`})
@@ -79,19 +84,23 @@ $(document).ready(function () {
     }
   };
 
+  //randomize and select 4 activities so users don't see the same each time
   function randomizeActivities (activityList) {
    randomizeList = activityList.sort(function(a, b){return 0.5 - Math.random()}).slice(0,4)
    return randomizeList
  }
 
+
 // find places from google places
+  var map
+  var service
   function getPlaces() {
     $('#placeResults').css('display', 'block');
     //set type to search as category selected by user
     var categoryType = $(this).attr('id')
     //search by locations lat lng values
     var location = new google.maps.LatLng(lat,lng)
-    // must create map though it wont be displayed in this app
+    // create map though it wont be displayed
     map = new google.maps.Map(document.getElementById('map'), {})
     // assign properties to request
     var request = {
@@ -112,36 +121,35 @@ $(document).ready(function () {
         var place = results[i]
         // perform details search by place id based on results, call displayPlaces function
           service.getDetails({placeId: place.place_id}, displayPlaces)
-          $(`<div></div>`).addClass(`${place.place_id}`)
       }
     }
   }
   // display place search result details on page
   function displayPlaces (place, status) {
      var container = $('<div></div>').addClass('place')
-    if (place !==  'undefined' && place) {
-    $(`<p>${place.name}</p>`).appendTo(container)
-    $(`<p>${place.formatted_address}</p>`).appendTo(container)
-    if (place.website){
-    $(`<p>website: <a href="${place.website}">${place.website}</a></p>`).appendTo(container)
-    }
-      // (function stars () {
-        if (place.rating > 1){
-          $(`<p>${place.rating} stars</p>`).appendTo(container)
+      if (place !==  'undefined' && place) {
+      $(`<p>${place.name}</p>`).appendTo(container)
+      $(`<p>${place.formatted_address}</p>`).appendTo(container)
+      if (place.website){
+      $(`<p>website: <a href="${place.website}">${place.website}</a></p>`).appendTo(container)
         }
-        else if (place.rating === 1){
-          $(`<p>${place.rating} star</p>`).appendTo(container)
+      if (place.rating > 1){
+      $(`<p>${place.rating} stars</p>`).appendTo(container)
         }
-        else {$('<p>no stars yet</p>').appendTo(container)}
-      // })()
-    if (place.reviews[0] && place.reviews[0].text.length > 0) {
+      else if (place.rating === 1){
+      $(`<p>${place.rating} star</p>`).appendTo(container)
+        }
+      else {
+      $('<p>no stars yet</p>').appendTo(container)
+        }
+      if (place.reviews[0] && place.reviews[0].text.length > 0) {
       $(`<p>review: ${place.reviews[0].text}</p>`).appendTo(container)
-      }
-      container.appendTo('#placeResults')
+        }
+     container.appendTo('#placeResults')
     }
     scrollPlace()
   }
-
+  // scroll to place results
   function scrollPlace (){
     $('html, body').stop().animate({
         scrollTop: $('#placeResults').offset().top
